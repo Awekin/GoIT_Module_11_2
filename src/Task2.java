cf149f70e889b3d7e73caa0f699da640315941ec
Awekin <semeniyk007@gmail.com>
2026-05-25 14:27:27 +0200

    1

--- src/Task2.java
+++ src/Task2.java
@@ -1,13 +1,11 @@
+import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Task2 {
     public List<String> getSortedNames(List<String> names2) {
 
         return names2.stream()
                 .map(String::toUpperCase)
-                .sorted()
-                .collect(Collectors.toList())
-                .reversed();
+                .sorted(Comparator.reverseOrder()).toList();
     }
 }
